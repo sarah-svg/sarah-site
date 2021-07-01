93ced517f4c6df6cbfd71486561b7937bb6dfa6b
sarah-svg <kjskillen@gmail.com>
2021-07-01 09:12:40 -0700

    hey

---
+++ src/sparkel/Sparkle.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+import { Card } from 'sparkle-ui';
+import 'sparkle-ui/dist/index.css';
+
+export class Sparkle extends Component {
+  render() {
+    return <Card learnText="Learn More" alt="Samsung Camera" target="#">Lorem ipsum dolor asit amet.</Card>;
+  }
+}
