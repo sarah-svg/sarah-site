45007b4c6b99937bf4960a4b68bb30d065792fed
sarah-svg <kjskillen@gmail.com>
2021-07-01 09:15:55 -0700

    hry2

---
+++ src/tech/Tech.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Tech.scss';
+function Tech() {
+  return (
+    <div>
+      <span className="typewriter2"><h6>Tech Stack: JavaScript, CSS, HTML, React, Node, SQL, Express, and SASS</h6></span>  
+    </div>
+  );
+}
+
+export default Tech;
