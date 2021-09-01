26cd8c4de6f6d9af57053fdf606c26f6e3251b9d
sarah-svg <kjskillen@gmail.com>
2021-08-31 18:06:20 -0700

    fixxing the footer style

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -3,40 +3,36 @@ import './Contact.scss';
 
 export default function Contact() {
   return (
-    <div className="Contact" name="contact">
-      <div className="inner1">
-        <div className="content">
+    <>
+      <div className="Contact" name="contact">
+  
 
-          <p>Reach me at:</p>
+   
+        <div className="column">
+          <a href="mailto: kjskillen@gmail.com" >
+            <img src="./email.png" alt="icon link to Sarah's email" /></a>
+        </div>
+        <div className="column">
+          <a href="https://www.linkedin.com/in/sarah-skillen-7339b61b8/" >
+            <img src="./linkdin.png" alt="icon link to sarah's linkedin" /></a>
+        </div> 
+        <div className="column">
+          <a href="https://github.com/sarah-svg">
+            <img src="./git.png" alt="icon link to Sarah's github" /></a>
+        </div>   
+   
+        <span className="tooltip-text"></span>
+   
+      </div>   
+      <p> Created by Sarah Skillen &#169; 2021 </p> 
 
-          <p>
-            <a href="mailto: kjskillen@gmail.com">
-              <img src="./email.png" alt="icon link to Sarah's email" />
-                        kjskillen@gmail.com
-            </a>
-          </p>
-          <p>
-            <a href="https://www.linkedin.com/in/sarah-skillen-7339b61b8/">
-              <img src="./linkdin.png" alt="icon link to sarah's linkedin" />
-                        /in/sarahskillen
-            </a>
-          </p>
-          <p>
-            <a href="https://github.com/sarah-svg">
-              <img src="./git.png" alt="icon link to Sarah's github" />
-                        /sarah-svg
-            </a>
-          </p>
-          <p className="tooltip">
-            {/* <a href="./resume2.pdf">
+    </>
+  );
+}
+ 
+// eslint-disable-next-line no-lone-blocks
+{/* <a href="./resume2.pdf">/in/sarahskillen
+kjskillen@gmail.com /sarah-svg
               <img src="./resume2.png" alt="icon link to Sarah's resume" />
                         Resume
             </a> */}
-            <span className="tooltip-text"></span>
-          </p>
-        </div>
-
-      </div>
-    </div>
-  );
-}
