8939e5ffd2b14cc0b039de0f01b35cb75add3d63
sarah-svg <kjskillen@gmail.com>
2022-08-15 22:45:26 -0700

    more

--- src/Projects/ListProjects.jsx
+++ src/Projects/ListProjects.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { projectsData } from './data';
 import { Project } from './Project';
 import uuid from 'react-uuid';
