555a7d04d90480fa345e9f25de3eabd24fdcc8ba
sarah-svg <kjskillen@gmail.com>
2022-09-16 22:02:33 -0700

    remove import

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 
 import './Main.scss';
 import About from '../About/About';
@@ -11,18 +12,18 @@ function Main() {
     <div className='d-flex h-100 w-100 flex-grow-0 flex-wrap body'>
       <Header/>
       <div className="Main">
-        <div className="shine gradient-background">
-          <div className="Landing" name="home">
-            <div className="landing-group">
-              <div className="landing-header">
-                <div className="typewriter">
-                  <h1 className="name">Sarah Skillen</h1>
-                </div>
-              </div> 
-            </div>
-            <div className="arrow">      
-            </div>
+
+        <div className="Landing" name="home">
+          <div className="landing-group">
+            <div className="landing-header">
+              <div className="typewriter">
+                <h1 className="name">Sarah Skillen</h1>
+              </div>
+            </div> 
           </div>
+          <div className="arrow">      
+          </div>
+
           <About />     
         </div>    
       </div>
