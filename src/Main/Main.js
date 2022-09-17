e763aac65c45038fa923113ff8db2649900c2abb
sarah-svg <kjskillen@gmail.com>
2022-09-16 22:07:09 -0700

    here

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -12,18 +12,8 @@ function Main() {
     <div className='d-flex h-100 w-100 flex-grow-0 flex-wrap body'>
       <Header/>
       <div className="Main">
-
-        <div className="Landing" name="home">
-          <div className="landing-group">
-            <div className="landing-header">
-              <div className="typewriter">
-                <h1 className="name">Sarah Skillen</h1>
-              </div>
-            </div> 
-          </div>
-          <div className="arrow">      
-          </div>
-
+        <div className="typewriter">
+          <h1 className="name">Sarah Skillen</h1>
           <About />     
         </div>    
       </div>
