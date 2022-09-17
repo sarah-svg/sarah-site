a619019bb2a28da78372cb83368761e88c76d97a
sarah-svg <kjskillen@gmail.com>
2022-09-17 12:01:24 -0700

    wording change

--- src/About/About.js
+++ src/About/About.js
@@ -8,9 +8,9 @@ export default function About() {
 
       <div className="p"> 
        I am a hardworking creative <span className="job">Software Engineer</span> who loves the outdoors and solving problems.
-        After working as a cosmotologist for the past decade, I am a people person who has a passion for code, and learning as much as I can.
-        I have a amazing skill of talking and understanding my surroundings and making it creative and learning at the same time. Creative,
-        transformative go getter and problem solver, vicarious team builder, hopeful, and a open collaborator.
+       After working as a cosmetologist for the past decade, I am a people person who has a passion for code and learning as much as I can.
+        I have an amazing skill of talking and understanding my surroundings and making it creative and learning at the same time. Creative,
+        transformative go-getter and problem solver, vicarious team builder, hopeful, and an open collaborator.
       </div>
       <Frameworks/>
     </div>
