f12287567639567bcbc21faec98383b3aa455afb
sarah-svg <kjskillen@gmail.com>
2022-09-19 20:44:58 -0700

    new blurb

--- src/About/About.js
+++ src/About/About.js
@@ -8,9 +8,7 @@ export default function About() {
 
       <div className="p"> 
        I am a hardworking creative <span className="job">Software Engineer</span> who loves the outdoors and solving problems.
-       After working as a cosmetologist for the past decade, I am a people person who has a passion for code and learning as much as I can.
-        I have an amazing skill of talking and understanding my surroundings and making it creative and learning at the same time. Creative,
-        transformative go-getter and problem solver, vicarious team builder, hopeful, and an open collaborator.
+       After working as a cosmetologist for over a decade and then transitioning to becoming a Software Engineer. I have an eye for detail, the skills to adapt and overcome anything, and a passion to learn as much as I can. 
       </div>
       <Frameworks/>
     </div>
