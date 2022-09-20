849ba8edca63f630f0e3a2e46fdeb4cd007cf59a
sarah-svg <kjskillen@gmail.com>
2022-09-20 12:56:03 -0700

    trying to get the framework style rigth

--- src/Main/framworks.js
+++ src/Main/framworks.js
@@ -6,19 +6,24 @@ import './Main.scss';
 function Frameworks() {
   return (
     <ul className="container d-flex flex-row-wrap">
-      <li className="card d-flex">
+      <li className="card d-flex" style={{ height:' 60px !important',
+        width:' 65px !important' }}>
         <img src='./react.svg' alt='react' className='react m-1' />
       </li>
-      <li className="card d-flex ">
+      <li className="card d-flex " style={{ height:' 60% !important',
+        width:' 65p% !important' }}>
         <img src='./javascript.svg' alt='javascript' className='javascript m-1' />
       </li>
-      <li className="card d-flex">
+      <li className="card d-flex" style={{ height:' 60px !important',
+        width:' 65px !important' }}>
         <img src='./node.svg' alt='node' className='node m-1' />
       </li> 
-      <li className="card d-flex ">
+      <li className="card d-flex " style={{ height:' 60px !important',
+        width:' 65px !important' }}>
         <img src='./css.svg' alt='css' className='css m-1' />
       </li>
-      <li className="card d-flex">
+      <li className="card d-flex" style={{ height:' 60px !important',
+        width:' 65px !important' }}>
         <img src='./sass.svg' alt='scss' className='scss m-1' />
       </li>
     </ul>
