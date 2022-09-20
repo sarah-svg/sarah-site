df4803e3495f145ec1ade191b3d49bde5b050e37
sarah-svg <kjskillen@gmail.com>
2022-09-19 23:24:57 -0700

    changes

--- src/Main/framworks.js
+++ src/Main/framworks.js
@@ -5,20 +5,20 @@ import './Main.scss';
 
 function Frameworks() {
   return (
-    <ul class="container">
-      <li class="card d-flex  w-100">
+    <ul className="container">
+      <li className="card d-flex  w-100">
         <img src='./react.svg' alt='react' className='react' />
       </li>
-      <li class="card d-flex  w-100">
+      <li className="card d-flex  w-100">
         <img src='./javascript.svg' alt='javascript' className='javascript' />
       </li>
-      <li class="card d-flex  w-100">
+      <li className="card d-flex  w-100">
         <img src='./node.svg' alt='node' className='node' />
       </li> 
-      <li class="card d-flex  w-100">
+      <li className="card d-flex  w-100">
         <img src='./css.svg' alt='css' className='css' />
       </li>
-      <li class="card d-flex  w-100">
+      <li className="card d-flex  w-100">
         <img src='./sass.svg' alt='scss' className='scss' />
       </li>
     </ul>
